da9fdebeda1427d4ec2eaccae84f89e561c1e148
Fife <39037648+Chris6170@users.noreply.github.com>
2019-03-06 08:29:42 -0500

    update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var express = require("express");
 
 var PORT = process.env.PORT || 8000;
 var app = express();
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("public"));
 app.listen(PORT, function() {
   console.log("Listening on port:%s", PORT);
 });
